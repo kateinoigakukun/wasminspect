8e53020a8acaece2c9588b7c3d82850991f72b3d
Yuta Saito <kateinoigakukun@gmail.com>
2019-12-28 11:17:56 +0900

    generalize test script

--- tests/simple_example.rs
+++ tests/simple_example.rs
@@ -3,15 +3,21 @@ use wasminspect_core::interpreter::*;
 
 use std::path::Path;
 
-#[test]
-fn add_example() {
+fn run_wasm(filename: &str, func: &str, args: Vec<WasmValue>, results: Vec<WasmValue>) {
     let example_dir = Path::new(file!()).parent().unwrap().join("simple-example");
-    let instance = WasmInstance::new(example_dir.join("calc.wasm").to_str().unwrap().to_string());
-    match instance.run(
-        Some("add".to_string()),
-        vec![WasmValue::I32(1), WasmValue::I32(2)],
-    ) {
-        Ok(result) => assert_eq!(result[0], WasmValue::I32(3)),
+    let instance = WasmInstance::new(example_dir.join(filename).to_str().unwrap().to_string());
+    match instance.run(Some(func.to_string()), args) {
+        Ok(result) => assert_eq!(result, results),
         Err(err) => panic!(err.message()),
     }
+}
+
+#[test]
+fn test_calc() {
+    run_wasm(
+        "calc.wasm",
+        "add",
+        vec![WasmValue::I32(1), WasmValue::I32(2)],
+        vec![WasmValue::I32(3)],
+    )
 }
