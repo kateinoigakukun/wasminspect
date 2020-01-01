d8873a1e97f78b19d76512a65e8f9de0c9ec8f40
Yuta Saito <kateinoigakukun@gmail.com>
2020-01-01 15:27:47 +0900

    48 passed; 25 failed;

--- wast-spec/src/spectest.rs
+++ wast-spec/src/spectest.rs
@@ -1,9 +1,24 @@
-use wasminspect_core::interpreter::{HostValue, WasmValue};
 use std::collections::HashMap;
+use wasminspect_core::interpreter::{HostValue, WasmValue};
 
 pub fn instantiate_spectest() -> HashMap<String, HostValue> {
     let mut module = HashMap::new();
     module.insert("memory".to_string(), HostValue::Mem());
-    module.insert("global_i32".to_string(), HostValue::Global(WasmValue::I32(666)));
+    module.insert(
+        "global_i32".to_string(),
+        HostValue::Global(WasmValue::I32(666)),
+    );
+    module.insert(
+        "global_i64".to_string(),
+        HostValue::Global(WasmValue::I32(666)),
+    );
+    module.insert(
+        "global_f32".to_string(),
+        HostValue::Global(WasmValue::F32(f32::from_bits(0x44268000))),
+    );
+    module.insert(
+        "global_f64".to_string(),
+        HostValue::Global(WasmValue::F64(f64::from_bits(0x4084d00000000000))),
+    );
     module
 }
