13a7263d1b483a7f031d5885a28930de70df399e
Yuta Saito <kateinoigakukun@gmail.com>
2020-01-15 20:11:27 +0900

    wip

--- crates/cli/src/dwarf.rs
+++ crates/cli/src/dwarf.rs
@@ -1,5 +1,14 @@
-use parity_wasm;
-pub fn parse_dwarf(module: &parity_wasm::elements::Module) {
+use gimli::{DebugStr, DebugAbbrev, DebugInfo, DebugLine, LittleEndian};
+use parity_wasm::elements::{Module};
+use std::collections::HashMap;
+pub fn parse_dwarf(module: &Module) {
+    let mut sections = HashMap::new();
     for section in module.custom_sections() {
+        sections.insert(section.name(), section.payload());
     }
+    let endian = LittleEndian;
+    let debug_str = DebugStr::new(sections.get(".debug_str").unwrap(), endian);
+    let debug_abbrev = DebugAbbrev::new(sections.get(".debug_abbrev").unwrap(), endian);
+    let debug_info = DebugInfo::new(sections.get(".debug_info").unwrap(), endian);
+    let debug_line = DebugLine::new(sections.get(".debug_line").unwrap(), endian);
 }
