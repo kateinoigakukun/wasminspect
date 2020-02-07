75a2792c475fdce7839ef168181d48987ee6d9e5
Yuta Saito <kateinoigakukun@gmail.com>
2020-02-07 19:01:00 +0900

    fix 'list' underline bug

--- crates/cli/src/commands/disassemble.rs
+++ crates/cli/src/commands/disassemble.rs
@@ -19,7 +19,7 @@ impl<D: Debugger> Command<D> for DisassembleCommand {
     fn run(&self, debugger: &mut D, _context: &CommandContext, _args: Vec<&str>) -> Result<()> {
         let (insts, next_index) = debugger.instructions()?;
         for (index, inst) in insts.iter().enumerate() {
-            let prefix = if index == next_index { "->" } else { "  " };
+            let prefix = if index + 1 == next_index { "->" } else { "  " };
             println!("{} 0x{:>08x}: {:?}", prefix, inst.offset, inst.kind)
         }
         Ok(())
--- crates/cli/src/commands/list.rs
+++ crates/cli/src/commands/list.rs
@@ -40,25 +40,29 @@ pub fn display_source(line_info: LineInfo) -> Result<()> {
     use std::io::{BufRead, BufReader};
     let source = BufReader::new(File::open(line_info.filepath)?);
     for (index, line) in source.lines().enumerate() {
+        // line_info.line begin with 1
+        let index = index + 1;
+        let line = line?;
+
         let out = if Some(index as u64) == line_info.line {
-            let mut out = format!("-> {} ", index);
+            let mut out = format!("-> {: <4} ", index);
             match line_info.column {
                 ColumnType::Column(col) => {
-                    for (col_index, col_char) in line.iter().enumerate() {
-                        if col_index as u64 == col {
-                            out = format!("{}\x1B[4;34m{}\x1B[0m", out, col_char);
+                    for (col_index, col_char) in line.chars().enumerate() {
+                        if (col_index + 1) as u64 == col {
+                            out = format!("{}\x1B[4m{}\x1B[0m", out, col_char);
                         } else {
                             out = format!("{}{}", out, col_char);
                         }
                     }
                 }
                 ColumnType::LeftEdge => {
-                    out = format!("{}{}", out, line?);
+                    out = format!("{}{}", out, line);
                 }
             }
             out
         } else {
-            format!("   {} {}", index, line?)
+            format!("   {: <4} {}", index, line)
         };
         println!("{}", out);
     }
