6c595b8a2e61abb9184214b156c2197a73db7acc
Yuta Saito <kateinoigakukun@gmail.com>
2021-03-27 14:08:47 +0900

    add simple test case

--- crates/debugger-server/src/lib.rs
+++ crates/debugger-server/src/lib.rs
@@ -1,7 +1,6 @@
 mod rpc;
+mod socket;
 
-use anyhow::anyhow;
-use futures::{FutureExt, SinkExt, StreamExt};
 use headers::{
     Connection, Header, HeaderMapExt, SecWebsocketAccept, SecWebsocketKey, SecWebsocketVersion,
     Upgrade,
@@ -16,8 +15,6 @@ use hyper::{
     Method, Request, StatusCode,
 };
 use tokio;
-use tokio_tungstenite::tungstenite::{protocol, Message};
-use tokio_tungstenite::WebSocketStream;
 use wasminspect_debugger::*;
 
 use std::{cell::RefCell, net::SocketAddr, rc::Rc};
@@ -34,96 +31,6 @@ pub async fn start(addr: SocketAddr) {
 }
 
 // let mut data: DebuggerRequest = serde_json::from_reader(body.reader())?;
-async fn establish_connection(upgraded: Upgraded) -> Result<(), anyhow::Error> {
-    let mut ws = WebSocketStream::from_raw_socket(upgraded, protocol::Role::Server, None).await;
-    use std::sync::mpsc::{self, Receiver, Sender};
-    let (request_tx, request_rx): (Sender<Option<Message>>, Receiver<Option<Message>>) =
-        mpsc::channel();
-
-    tokio::task::spawn(async move {
-        let (process, dbg_context) = wasminspect_debugger::start_debugger(None).unwrap();
-        let context = Rc::new(Context {
-            process: RefCell::new(process),
-            dbg_context: RefCell::new(dbg_context),
-        });
-
-        match init_process(context.as_ref()) {
-            Ok(_) => {}
-            Err(err) => eprintln!("{}", err),
-        }
-        loop {
-            let msg = match request_rx.recv() {
-                Ok(Some(msg)) => msg,
-                Ok(None) => break,
-                Err(err) => {
-                    log::error!("Receiving error: {}", err);
-                    break;
-                }
-            };
-        }
-    });
-    while let Some(msg) = ws.next().await {
-        request_tx.send(Some(msg?))?;
-    }
-    request_tx.send(None)?;
-
-    Ok(())
-}
-async fn socket_handshake(req: Request<Body>) -> Result<Response<Body>, anyhow::Error> {
-    fn try_get_header<H>(req: &Request<Body>) -> Result<H, anyhow::Error>
-    where
-        H: Header,
-    {
-        match req.headers().typed_get::<H>() {
-            Some(header_value) => Ok(header_value),
-            None => {
-                return Err(anyhow!(format!(
-                    "Missing request header {}",
-                    H::name().as_str()
-                )));
-            }
-        }
-    }
-    let upgrade_to = try_get_header::<Upgrade>(&req)?;
-    if upgrade_to != Upgrade::websocket() {
-        return Err(anyhow!("Invalid request header value in UPGRADE"));
-    }
-
-    let ws_version = try_get_header::<SecWebsocketVersion>(&req)?;
-    if ws_version != SecWebsocketVersion::V13 {
-        return Err(anyhow!(format!(
-            "Unsupported WebSocket version: {:?}",
-            ws_version
-        )));
-    }
-
-    let ws_key = try_get_header::<SecWebsocketKey>(&req)?;
-    let upgrade = hyper::upgrade::on(req);
-    tokio::task::spawn(async move {
-        match upgrade.await {
-            Ok(upgraded) => match establish_connection(upgraded).await {
-                Ok(_) => {}
-                Err(err) => {
-                    log::error!("error while connection: {}", err);
-                }
-            },
-            Err(err) => {
-                log::error!("upgrade error: {}", err);
-            }
-        }
-    });
-
-    let mut res = Response::builder()
-        .status(StatusCode::SWITCHING_PROTOCOLS)
-        .body(Body::empty())
-        .unwrap();
-
-    res.headers_mut().typed_insert(Connection::upgrade());
-    res.headers_mut().typed_insert(Upgrade::websocket());
-    res.headers_mut()
-        .typed_insert(SecWebsocketAccept::from(ws_key));
-    Ok(res)
-}
 
 async fn remote_api(
     req: Request<Body>,
@@ -142,7 +49,7 @@ async fn remote_api(
                 .load_module(&wasm_bytes)?;
             Ok(Response::new(Body::empty()))
         }
-        (&Method::GET, "debug") => socket_handshake(req).await,
+        (&Method::GET, "debug") => socket::socket_handshake(req, |upgraded| async { Ok(()) }).await,
         _ => {
             // Return 404 not found response.
             Ok(Response::builder()
---
+++ crates/debugger-server/src/socket.rs
@@ -0,0 +1,211 @@
+use anyhow::anyhow;
+use futures::{FutureExt, SinkExt, StreamExt};
+
+use headers::{
+    Connection, Header, HeaderMapExt, SecWebsocketAccept, SecWebsocketKey, SecWebsocketVersion,
+    Upgrade,
+};
+use hyper::{
+    header::{self, AsHeaderName, HeaderName, ToStrError},
+    upgrade::OnUpgrade,
+    upgrade::Upgraded,
+    Body, Response, Server,
+};
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Method, Request, StatusCode,
+};
+
+use tokio_tungstenite::tungstenite::{protocol, Message};
+use tokio_tungstenite::WebSocketStream;
+
+use std::{cell::RefCell, net::SocketAddr, rc::Rc};
+
+pub async fn socket_handshake<F, Fut>(
+    req: Request<Body>,
+    connect: F,
+) -> Result<Response<Body>, anyhow::Error>
+where
+    F: Send + 'static + FnOnce(Upgraded) -> Fut,
+    Fut: std::future::Future<Output = Result<(), anyhow::Error>> + Send,
+{
+    fn try_get_header<H>(req: &Request<Body>) -> Result<H, anyhow::Error>
+    where
+        H: Header,
+    {
+        match req.headers().typed_get::<H>() {
+            Some(header_value) => Ok(header_value),
+            None => {
+                return Err(anyhow!(format!(
+                    "Missing request header {}",
+                    H::name().as_str()
+                )));
+            }
+        }
+    }
+    let upgrade_to = try_get_header::<Upgrade>(&req)?;
+    if upgrade_to != Upgrade::websocket() {
+        return Err(anyhow!("Invalid request header value in UPGRADE"));
+    }
+
+    let ws_version = try_get_header::<SecWebsocketVersion>(&req)?;
+    if ws_version != SecWebsocketVersion::V13 {
+        return Err(anyhow!(format!(
+            "Unsupported WebSocket version: {:?}",
+            ws_version
+        )));
+    }
+
+    let ws_key = try_get_header::<SecWebsocketKey>(&req)?;
+    let upgrade = hyper::upgrade::on(req);
+    tokio::spawn(async move {
+        match upgrade.await {
+            Ok(upgraded) => match connect(upgraded).await {
+                Ok(_) => {}
+                Err(err) => {
+                    log::error!("error while connection: {}", err);
+                }
+            },
+            Err(err) => {
+                log::error!("upgrade error: {}", err);
+            }
+        }
+    });
+
+    let mut res = Response::builder()
+        .status(StatusCode::SWITCHING_PROTOCOLS)
+        .body(Body::empty())
+        .unwrap();
+
+    res.headers_mut().typed_insert(Connection::upgrade());
+    res.headers_mut().typed_insert(Upgrade::websocket());
+    res.headers_mut()
+        .typed_insert(SecWebsocketAccept::from(ws_key));
+    Ok(res)
+}
+
+async fn establish_connection(upgraded: Upgraded) -> Result<(), anyhow::Error> {
+    let mut ws = WebSocketStream::from_raw_socket(upgraded, protocol::Role::Server, None).await;
+    use std::sync::mpsc::{self, Receiver, Sender};
+    let (request_tx, request_rx): (Sender<Option<Message>>, Receiver<Option<Message>>) =
+        mpsc::channel();
+
+    // tokio::task::spawn(async move {
+    //     let (process, dbg_context) = wasminspect_debugger::start_debugger(None).unwrap();
+    //     let context = Rc::new(Context {
+    //         process: RefCell::new(process),
+    //         dbg_context: RefCell::new(dbg_context),
+    //     });
+
+    //     match init_process(context.as_ref()) {
+    //         Ok(_) => {}
+    //         Err(err) => eprintln!("{}", err),
+    //     }
+    //     loop {
+    //         let msg = match request_rx.recv() {
+    //             Ok(Some(msg)) => msg,
+    //             Ok(None) => break,
+    //             Err(err) => {
+    //                 log::error!("Receiving error: {}", err);
+    //                 break;
+    //             }
+    //         };
+    //     }
+    // });
+    while let Some(msg) = ws.next().await {
+        request_tx.send(Some(msg?))?;
+    }
+    request_tx.send(None)?;
+
+    Ok(())
+}
+
+mod tests {
+    use std::{pin::Pin, sync::Arc, task::Poll, thread::sleep, time::Duration};
+
+    use futures::{channel::oneshot, TryFutureExt};
+    use futures::{task, Future};
+    use hyper::{server::conn::Http, Uri};
+    use tokio::net::{TcpListener, TcpStream as TkTcpStream};
+    use tokio_tungstenite::tungstenite::{protocol, Message};
+    use tokio_tungstenite::WebSocketStream;
+
+    use super::*;
+
+    #[derive(Clone)]
+    struct AddrConnect(SocketAddr);
+
+    impl tower_service::Service<hyper::http::Uri> for AddrConnect {
+        type Response = ::tokio::net::TcpStream;
+        type Error = ::std::io::Error;
+        type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+        fn poll_ready(&mut self, _cx: &mut task::Context<'_>) -> Poll<Result<(), Self::Error>> {
+            Poll::Ready(Ok(()))
+        }
+
+        fn call(&mut self, _: hyper::http::Uri) -> Self::Future {
+            Box::pin(tokio::net::TcpStream::connect(self.0))
+        }
+    }
+
+    fn tcp_bind(addr: &SocketAddr) -> ::tokio::io::Result<TcpListener> {
+        use std::net::TcpListener as StdTcpListener;
+        let std_listener = StdTcpListener::bind(addr).unwrap();
+        std_listener.set_nonblocking(true).unwrap();
+        TcpListener::from_std(std_listener)
+    }
+
+    async fn echo(upgraded: Upgraded) -> anyhow::Result<()> {
+        let ws = WebSocketStream::from_raw_socket(upgraded, protocol::Role::Server, None).await;
+        let (tx, rx) = ws.split();
+        rx.inspect(|i| log::debug!("ws recv: {:?}", i))
+            .forward(tx)
+            .await?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_socket_handshake() {
+        let _ = env_logger::try_init();
+
+        let listener = tcp_bind(&"127.0.0.1:0".parse().unwrap()).unwrap();
+        let addr = listener.local_addr().unwrap();
+        let (upgraded_tx, upgraded_rx) = oneshot::channel::<Upgraded>();
+
+        tokio::spawn(async move {
+            let uri: Uri = format!("http://{}", addr).parse().expect("valid URI");
+            let req = Request::builder()
+                .uri(uri)
+                .header("connection", "upgrade")
+                .header("upgrade", "websocket")
+                .header("sec-websocket-version", "13")
+                .header("sec-websocket-key", "dGhlIHNhbXBsZSBub25jZQ==")
+                .body(Body::empty())
+                .expect("connection req");
+            let upgrade = ::hyper::Client::builder()
+                .build(AddrConnect(addr))
+                .request(req)
+                .and_then(|res| hyper::upgrade::on(res));
+            match upgrade.await {
+                Ok(up) => upgraded_tx.send(up).expect("send upgraded"),
+                Err(err) => {
+                    panic!("{}", err);
+                }
+            };
+        });
+        let svc = service_fn(|req| socket_handshake(req, echo));
+        let (socket, _) = listener.accept().await.unwrap();
+        Http::new()
+            .serve_connection(socket, svc)
+            .with_upgrades()
+            .await
+            .unwrap();
+        let upgraded = upgraded_rx.await.expect("recv upgraded");
+        let mut ws = WebSocketStream::from_raw_socket(upgraded, protocol::Role::Client, None).await;
+        let msg = Message::Text("hello".to_string());
+        ws.send(msg.clone()).await.expect("send msg");
+        let recv = ws.next().await.expect("recv msg").unwrap();
+        assert_eq!(recv, msg);
+    }
+}
