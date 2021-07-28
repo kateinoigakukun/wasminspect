eccaa842ea1e0ef600f8f4d485c45ec34c9f5ec0
Yuta Saito <kateinoigakukun@gmail.com>
2021-07-28 20:01:36 +0900

    Pin development tools versions by Nix

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let
+  oxalica_rust_overlay = import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/f2865d93afeab55ba158717dda47ef38dec331f7.tar.gz);
+  pkgs = import <nixpkgs> {
+    overlays = [ oxalica_rust_overlay ];
+  };
+  rust_toolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
+in
+  with pkgs;
+
+  pkgs.mkShell rec {
+    nativeBuildInputs = lib.optionals stdenv.isDarwin [
+      darwin.libiconv
+    ] ++ [
+      rust_toolchain
+      curl
+    ];
+  }
